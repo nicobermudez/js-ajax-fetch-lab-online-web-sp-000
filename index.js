046a091b156e0cced4601bdb522acd31d467220e
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-08 17:26:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const baseURL = 'https://api.github.com'
+const user = 'nicobermudez'
+
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
