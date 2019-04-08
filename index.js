30376ad773dce50062d988ed359a94b26983492d
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-08 17:38:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function getToken() {
 function forkRepo() {
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
   //use fetch to fork it!
-  const url = `${baseURL}/repos/${repo}/forks`
+  const url = `${baseURL}/repos/${repo}/forks`;
   fetch(url, {
     method: 'POST'
     headers: {
